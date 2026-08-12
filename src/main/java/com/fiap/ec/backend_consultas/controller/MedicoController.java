f6add869c107ae319782f04f1d8904b80d40c6fd
pedro-alvarez <p.alvarezcerto@gmail.com.br>
2026-08-11 22:40:51 -0300

    aula 05/05

---
+++ src/main/java/com/fiap/ec/backend_consultas/controller/MedicoController.java
@@ -0,0 +1,47 @@
+package com.fiap.ec.backend_consultas.controller;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fiap.ec.backend_consultas.model.Medico;
+import com.fiap.ec.backend_consultas.service.MedicoService;
+@RestController
+@RequestMapping("/medicos")
+@CrossOrigin
+public class MedicoController {
+    private final MedicoService service;
+    public MedicoController(MedicoService service) {
+        this.service = service;
+    }
+    @PostMapping
+    public Medico salvar(@RequestBody Medico medico) {
+        return service.salvar(medico);
+    }
+    @GetMapping
+    public List<Medico> listar() {
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Medico buscarPorId(@PathVariable Long id) {
+        return service.buscarPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public Medico atualizar(@PathVariable Long id, @RequestBody Medico medico) {
+        return service.atualizar(id, medico);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        service.deletar(id);
+    }
+}
